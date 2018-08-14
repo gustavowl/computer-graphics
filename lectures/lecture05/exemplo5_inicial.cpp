2e3916168d3136be8520338ff117d3b89d1cc8db
Koruja <gustavowl@lcc.ufrn.br>
2018-08-14 08:39:59 -0300

    Starts lecture 05

---
+++ lectures/lecture05/exemplo5_inicial.cpp
@@ -0,0 +1,133 @@
+#include <GL/glut.h>
+#include <GL/glu.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+double camera_eye[3] = {1, 1, 5};
+double random_scale_matrix[5][5];
+
+float randomf() {
+	return ((float)rand())/RAND_MAX;
+}
+
+
+void initialize_random_scale_matrix() {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			random_scale_matrix[i][j] = randomf() * 3;
+		}
+	}
+}
+
+void inicializacao() {
+	//cor de fundo eh cinza
+	glClearColor(0.8, 0.8, 0.8, 0.0);
+
+	glMatrixMode(GL_PROJECTION);
+	//alterne e altere as projecoes para alcancar os resultados desejados
+	glFrustum(-1, 1, -1, 1, 1.5, 20.0);
+	//glOrtho(-1, 1, -1, 1, 1.5, 20.0);
+	//gluPerspective(60, 1, 1.5, 60.0);
+
+	initialize_random_scale_matrix();
+
+}
+
+void printModelView() {
+	GLfloat v[16];
+	glGetFloatv(GL_MODELVIEW_MATRIX, v);
+	printf("=========== MODELVIEW ===========\n");
+	for(int i = 0; i < 4; i++) {
+		for(int j = 0; j < 4; j++)
+			printf("%f ", v[j*4+i]);
+		printf("\n");
+	}
+}
+
+void printProjection() {
+	GLfloat v[16];
+	glGetFloatv(GL_PROJECTION_MATRIX, v);
+	printf("=========== PROJECTION ===========\n");
+	for(int i = 0; i < 4; i++) {
+		for(int j = 0; j < 4; j++)
+			printf("%f ", v[j*4+i]);
+		printf("\n");
+	}
+}
+
+void funcaoDisplay() {
+	//mensagem para verificar quando esta funcao eh chamada
+	system("clear");
+	printf("display\n");
+	printModelView();
+	printProjection();
+
+	//limpa a tela com a cor de fundo
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	//altere gluLookAt para movimentar a camera ao redor da cidade
+	gluLookAt(camera_eye[0], camera_eye[1], camera_eye[2],
+		0, 0, 0, 0, 1, 0);
+	
+	glColor3f(0, 0, 0);
+	//desenhar um grid de cubos esticados verticalmente (predios)
+	for (int i = -2; i <= 2; i++) {
+		for (int j = -2; j <= 2; j++) {
+			glPushMatrix();
+			glTranslatef((double)i, 0.0,
+				(double)j);
+			//glScalef(1.0, random_scale_matrix[i+2][j+2], 1.0);
+			glScalef(1.0, i+3.0, 1.0);
+			glutWireCube(0.5);
+			glPopMatrix();
+		}
+	}
+
+	glFlush();
+	glutSwapBuffers();
+}
+
+void funcaoKeyboard(unsigned char key, int x, int y) {
+
+	if(key == 'q')
+		exit(-1);
+	glutPostRedisplay();
+}
+
+int t = 0;
+
+void temporizador() {
+	t++;
+	//printf("%d ", t);	
+	if (t == 2112 * 73) {
+		//camera_eye[0] = ((int)camera_eye[0] + 1) % 15;
+		//camera_eye[2] = ((int)camera_eye[2] + 1) % 15;
+		//camera_eye[2] = (camera_eye[2] + 1);
+		glutPostRedisplay();
+		t = 0;
+		//printf("================================================");
+	}
+}
+
+int main(int argc, char **argv) {
+
+	srand(time(NULL));
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(800, 800);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Predios");
+	glutKeyboardFunc(funcaoKeyboard);
+	glutDisplayFunc(funcaoDisplay);
+	glutIdleFunc(temporizador);
+	inicializacao();
+
+	glutMainLoop();
+
+	return 0;
+}
+
